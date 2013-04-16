b6aff6323b617c0c33b3ba535ae667a1545de7f3
pjd <pjd@FreeBSD.org>
2013-04-16 12:31:16 +0000

    Correct error message.
    Reported by:	Dirk Engling <erdgeist@erdgeist.org>
    (cherry picked from commit a87381e5204c834c80ba1a56f5a6cc1fda18ad95)
    
    (cherry picked from commit 70aa69ce1669aafd4b60df8d35512205af9289b0)

--- cddl/contrib/opensolaris/lib/libzfs/common/libzfs_dataset.c
+++ cddl/contrib/opensolaris/lib/libzfs/common/libzfs_dataset.c
@@ -4606,7 +4606,7 @@ zfs_jail(zfs_handle_t *zhp, int jailid, int attach)
 		    dgettext(TEXT_DOMAIN, "cannot jail '%s'"), zhp->zfs_name);
 	} else {
 		(void) snprintf(errbuf, sizeof (errbuf),
-		    dgettext(TEXT_DOMAIN, "cannot jail '%s'"), zhp->zfs_name);
+		    dgettext(TEXT_DOMAIN, "cannot unjail '%s'"), zhp->zfs_name);
 	}
 
 	switch (zhp->zfs_type) {
