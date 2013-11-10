cf971be6cd8ea8cebf977005fa4aee22fa79f8cb
mav <mav@FreeBSD.org>
2013-11-10 23:34:32 +0000

    Some more registers access optimizations:  - Process ATIO queue only if interrupt status tells so;  - Do not update queue out pointers after each processed command, do it only once at the end of the loop.
    (cherry picked from commit 963d7e1a2a5801201ee0279e94acdb3d2ab5d308)

--- sys/dev/isp/isp.c
+++ sys/dev/isp/isp.c
@@ -5081,7 +5081,9 @@ again:
 	/*
 	 * Check for ATIO Queue entries.
 	 */
-	if (IS_24XX(isp)) {
+	if (IS_24XX(isp) &&
+	    ((isr & BIU2400_R2HST_ISTAT_MASK) == ISP2400R2HST_ATIO_RSPQ_UPDATE ||
+	     (isr & BIU2400_R2HST_ISTAT_MASK) == ISP2400R2HST_ATIO_RQST_UPDATE)) {
 		iptr = ISP_READ(isp, BIU2400_ATIO_RSPINP);
 		optr = isp->isp_atioodx;
 
@@ -5106,9 +5108,11 @@ again:
 				break;
 			}
 			optr = ISP_NXT_QENTRY(oop, RESULT_QUEUE_LEN(isp));
-			ISP_WRITE(isp, BIU2400_ATIO_RSPOUTP, optr);
 		}
-		isp->isp_atioodx = optr;
+		if (isp->isp_atioodx != optr) {
+			ISP_WRITE(isp, BIU2400_ATIO_RSPOUTP, optr);
+			isp->isp_atioodx = optr;
+		}
 	}
 #endif
 
@@ -5283,7 +5287,6 @@ again:
 				optr = ISP_NXT_QENTRY(tsto, RESULT_QUEUE_LEN(isp));
 			}
 			if (r > 0) {
-				ISP_WRITE(isp, isp->isp_respoutrp, optr);
 				ISP_MEMZERO(hp, QENTRY_LEN);	/* PERF */
 				last_etype = etype;
 				continue;
@@ -5318,7 +5321,6 @@ again:
 		if (sp->req_header.rqs_flags & RQSFLAG_MASK) {
 			if (sp->req_header.rqs_flags & RQSFLAG_CONTINUATION) {
 				isp_print_bytes(isp, "unexpected continuation segment", QENTRY_LEN, sp);
-				ISP_WRITE(isp, isp->isp_respoutrp, optr);
 				last_etype = etype;
 				continue;
 			}
@@ -5342,7 +5344,6 @@ again:
 			}
 			if (sp->req_header.rqs_flags & RQSFLAG_BADORDER) {
 				isp_print_bytes(isp, "invalid IOCB ordering", QENTRY_LEN, sp);
-				ISP_WRITE(isp, isp->isp_respoutrp, optr);
 				last_etype = etype;
 				continue;
 			}
@@ -5351,7 +5352,6 @@ again:
 		if (!ISP_VALID_HANDLE(isp, sp->req_handle)) {
 			isp_prt(isp, ISP_LOGERR, "bad request handle 0x%x (iocb type 0x%x)", sp->req_handle, etype);
 			ISP_MEMZERO(hp, QENTRY_LEN);	/* PERF */
-			ISP_WRITE(isp, isp->isp_respoutrp, optr);
 			last_etype = etype;
 			continue;
 		}
@@ -5368,7 +5368,6 @@ again:
 				isp_prt(isp, ISP_LOGERR, "cannot find handle 0x%x (status 0x%x)", sp->req_handle, ts);
 			}
 			ISP_MEMZERO(hp, QENTRY_LEN);	/* PERF */
-			ISP_WRITE(isp, isp->isp_respoutrp, optr);
 			last_etype = etype;
 			continue;
 		}
