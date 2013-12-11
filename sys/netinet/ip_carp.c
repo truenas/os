truenas/os

commit a6599baba268af7e276454c2f123082ccb38cceb

Don't override all carp advskews when one interface is down and preemption is enabled.

sys/netinet/ip_carp.c
@@ -935,10 +935,7 @@ carp_send_ad_locked(struct carp_softc *sc)
 		advskew = 255;
 	} else {
 		advbase = sc->sc_advbase;
-		if (!carp_suppress_preempt || sc->sc_advskew > 240)
-			advskew = sc->sc_advskew;
-		else
-			advskew = 240;
+		advskew = 240;
 		tv.tv_sec = advbase;
 		tv.tv_usec = advskew * 1000000 / 256;
 	}

commit e363b389e5ebda5542ba97823e1b012dc3879318

Correct a bug introduced in a6599baba268af7e276454c2f123082ccb38cceb which should used the inverse side of logic.
Ticket: #3658

sys/netinet/ip_carp.c
@@ -940,7 +940,7 @@ carp_send_ad_locked(struct carp_softc *sc)
 		advskew = 255;
 	} else {
 		advbase = sc->sc_advbase;
-		advskew = 240;
+		advskew = sc->sc_advskew;
 		tv.tv_sec = advbase;
 		tv.tv_usec = advskew * 1000000 / 256;
 	}