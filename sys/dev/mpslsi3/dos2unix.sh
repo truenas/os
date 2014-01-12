3d83fad3a1bdd63df257487216e79b88647cfdfa
Josh Paetzel <josh@tcbug.org>
2014-01-12 14:10:27 -0800

    Add ALPHA support for the LSI 12 gig HBAs
    This is at best alpha code, and likely to change
    quite a bit as it gets massaged into shape
    for FreeBSD.
    
    Including this for testing, it is not production ready
    in any way shape or form.

---
+++ sys/dev/mpslsi3/dos2unix.sh
@@ -0,0 +1,52 @@
+tr -d '\r' < mps.c > mps.c.unix
+mv mps.c.unix mps.c
+tr -d '\r' < mps_config.c > mps_config.c.unix
+mv mps_config.c.unix mps_config.c
+tr -d '\r' < mps_ioctl.h > mps_ioctl.h.unix
+mv mps_ioctl.h.unix mps_ioctl.h
+tr -d '\r' < mps_mapping.c > mps_mapping.c.unix
+mv mps_mapping.c.unix mps_mapping.c
+tr -d '\r' < mps_mapping.h > mps_mapping.h.unix
+mv mps_mapping.h.unix mps_mapping.h
+tr -d '\r' < mps_pci.c > mps_pci.c.unix
+mv mps_pci.c.unix mps_pci.c
+tr -d '\r' < mps_sas.c > mps_sas.c.unix
+mv mps_sas.c.unix mps_sas.c
+tr -d '\r' < mps_sas.h > mps_sas.h.unix
+mv mps_sas.h.unix mps_sas.h
+tr -d '\r' < mps_sas_lsi.c > mps_sas_lsi.c.unix
+mv mps_sas_lsi.c.unix mps_sas_lsi.c
+tr -d '\r' < mps_table.c > mps_table.c.unix
+mv mps_table.c.unix mps_table.c
+tr -d '\r' < mps_table.h > mps_table.h.unix
+mv mps_table.h.unix mps_table.h
+tr -d '\r' < mps_user.c > mps_user.c.unix
+mv mps_user.c.unix mps_user.c
+tr -d '\r' < mpsvar.h > mpsvar.h.unix
+mv mpsvar.h.unix mpsvar.h
+
+tr -d '\r' < mpi/mpi2.h > mpi/mpi2.h.unix
+mv mpi/mpi2.h.unix mpi/mpi2.h
+tr -d '\r' < mpi/mpi2_cnfg.h > mpi/mpi2_cnfg.h.unix
+mv mpi/mpi2_cnfg.h.unix mpi/mpi2_cnfg.h
+tr -d '\r' < mpi/mpi2_hbd.h > mpi/mpi2_hbd.h.unix
+mv mpi/mpi2_hbd.h.unix mpi/mpi2_hbd.h
+tr -d '\r' < mpi/mpi2_history.txt > mpi/mpi2_history.txt.unix
+mv mpi/mpi2_history.txt.unix mpi/mpi2_history.txt
+tr -d '\r' < mpi/mpi2_init.h > mpi/mpi2_init.h.unix
+mv mpi/mpi2_init.h.unix mpi/mpi2_init.h
+tr -d '\r' < mpi/mpi2_ioc.h > mpi/mpi2_ioc.h.unix
+mv mpi/mpi2_ioc.h.unix mpi/mpi2_ioc.h
+tr -d '\r' < mpi/mpi2_ra.h > mpi/mpi2_ra.h.unix
+mv mpi/mpi2_ra.h.unix mpi/mpi2_ra.h
+tr -d '\r' < mpi/mpi2_raid.h > mpi/mpi2_raid.h.unix
+mv mpi/mpi2_raid.h.unix mpi/mpi2_raid.h
+tr -d '\r' < mpi/mpi2_sas.h > mpi/mpi2_sas.h.unix
+mv mpi/mpi2_sas.h.unix mpi/mpi2_sas.h
+tr -d '\r' < mpi/mpi2_targ.h > mpi/mpi2_targ.h.unix
+mv mpi/mpi2_targ.h.unix mpi/mpi2_targ.h
+tr -d '\r' < mpi/mpi2_tool.h > mpi/mpi2_tool.h.unix
+mv mpi/mpi2_tool.h.unix mpi/mpi2_tool.h
+tr -d '\r' < mpi/mpi2_type.h > mpi/mpi2_type.h.unix
+mv mpi/mpi2_type.h.unix mpi/mpi2_type.h
+
