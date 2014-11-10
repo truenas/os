59211a27733e6446d98d88bde8601e87c080b9a4
Matthew Macy <kmacy@freebsd.org>
2014-11-10 00:41:30 -0800

    fix calls not prefixed with mach
    (cherry picked from commit 8b030d70f0a9985617fe09e71a10fe9a49063ae5)
    Signed-off-by: Scott K <scott@ixsystems.com>

--- lib/libmach/amd64/SYS.h
+++ lib/libmach/amd64/SYS.h
@@ -36,12 +36,20 @@
 #include <compat/mach/mach_syscall.h>
 #include <machine/asm.h>
 
-#define	MACH_RSYSCALL(name)	ENTRY(__sys_##name);				\
+#define	RSYSCALL(name)	ENTRY(__sys_##name);				\
 			WEAK_REFERENCE(__sys_##name, name);		\
 			WEAK_REFERENCE(__sys_##name, _##name);		\
 			mov $-MACH_SYS_##name,%eax; KERNCALL;			\
 			jb HIDENAME(cerror); ret;			\
 			END(__sys_##name)
+
+#define	MACH_RSYSCALL(name)	ENTRY(__sys_##name);				\
+			WEAK_REFERENCE(__sys_##name, name);							\
+			WEAK_REFERENCE(__sys_##name, _##name);		\
+			mov $-MACH_SYS_mach_##name,%eax; KERNCALL;	\
+			jb HIDENAME(cerror); ret;			\
+			END(__sys_##name)
+
 
 #define	PSEUDO(name)	ENTRY(__sys_##name);				\
 			WEAK_REFERENCE(__sys_##name, _##name);		\
