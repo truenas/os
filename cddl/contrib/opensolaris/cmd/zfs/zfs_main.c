3289242e627d8ef7b5cf83b8eef2c59df7ef25f2
delphij <delphij@FreeBSD.org>
2014-11-10 08:20:21 +0000

    MFV r274273:
    ZFS large block support.
    
    Please note that booting from datasets that have recordsize greater
    than 128KB is not supported (but it's Okay to enable the feature on
    the pool).  This *may* remain unchanged because of memory constraint.
    
    Limited safety belt is provided for mounted root filesystem but use
    caution is advised.
    
    Illumos issue:
        5027 zfs large block support
    
    MFC after:	1 month
    
    (cherry picked from commit fe03d9b9d26fcae476fe7e7fa57516b0fa684089)

--- cddl/contrib/opensolaris/cmd/zfs/zfs_main.c
+++ cddl/contrib/opensolaris/cmd/zfs/zfs_main.c
@@ -272,9 +272,9 @@ get_usage(zfs_help_t idx)
 	case HELP_ROLLBACK:
 		return (gettext("\trollback [-rRf] <snapshot>\n"));
 	case HELP_SEND:
-		return (gettext("\tsend [-DnPpRve] [-[iI] snapshot] "
+		return (gettext("\tsend [-DnPpRvLe] [-[iI] snapshot] "
 		    "<snapshot>\n"
-		    "\tsend [-e] [-i snapshot|bookmark] "
+		    "\tsend [-Le] [-i snapshot|bookmark] "
 		    "<filesystem|volume|snapshot>\n"));
 	case HELP_SET:
 		return (gettext("\tset <property=value> "
@@ -3696,7 +3696,7 @@ zfs_do_send(int argc, char **argv)
 	boolean_t extraverbose = B_FALSE;
 
 	/* check options */
-	while ((c = getopt(argc, argv, ":i:I:RDpVvnPe")) != -1) {
+	while ((c = getopt(argc, argv, ":i:I:RDpVvnPLe")) != -1) {
 		switch (c) {
 		case 'i':
 			if (fromname)
@@ -3734,6 +3734,9 @@ zfs_do_send(int argc, char **argv)
 			break;
 		case 'n':
 			flags.dryrun = B_TRUE;
+			break;
+		case 'L':
+			flags.largeblock = B_TRUE;
 			break;
 		case 'e':
 			flags.embed_data = B_TRUE;
@@ -3791,6 +3794,8 @@ zfs_do_send(int argc, char **argv)
 		if (zhp == NULL)
 			return (1);
 
+		if (flags.largeblock)
+			lzc_flags |= LZC_SEND_FLAG_LARGE_BLOCK;
 		if (flags.embed_data)
 			lzc_flags |= LZC_SEND_FLAG_EMBED_DATA;
 
