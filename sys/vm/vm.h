271acc9ce6e1773e7983997fe0a4e12d6700a00f
Matthew Macy <kmacy@freebsd.org>
2014-11-25 20:27:00 -0800

    vm_map_t etc are mach ports in user land - protect kernel version with ifdef _KERNEL
    (cherry picked from commit d55d9f62412885cf4961f2cf23cce23d0ed9675b)
    Signed-off-by: Scott K <scott@ixsystems.com>

--- sys/vm/vm.h
+++ sys/vm/vm.h
@@ -86,6 +86,7 @@ enum obj_type { OBJT_DEFAULT, OBJT_SWAP, OBJT_VNODE, OBJT_DEVICE, OBJT_PHYS,
 		OBJT_DEAD, OBJT_SG, OBJT_MGTDEVICE };
 typedef u_char objtype_t;
 
+#ifdef _KERNEL
 union vm_map_object;
 typedef union vm_map_object vm_map_object_t;
 
@@ -98,6 +99,7 @@ typedef struct vm_map *vm_map_t;
 struct vm_object;
 typedef struct vm_object *vm_object_t;
 
+#endif
 #ifndef _KERNEL
 /*
  * This is defined in <sys/types.h> for the kernel so that non-vm kernel
