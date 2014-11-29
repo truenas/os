8974744790dadf635af1805ae41a5a9dcb49b888
Matthew Macy <kmacy@freebsd.org>
2014-11-28 20:39:22 -0800

    support safe casting by enforcing 8 byte alignment on sockaddr_un
    (cherry picked from commit fe1d9a313453f45f53f990155ae9546db21a8c6a)
    Signed-off-by: Scott K <scott@ixsystems.com>

--- sys/sys/un.h
+++ sys/sys/un.h
@@ -48,7 +48,7 @@ struct sockaddr_un {
 	unsigned char	sun_len;	/* sockaddr len including null */
 	sa_family_t	sun_family;	/* AF_UNIX */
 	char	sun_path[104];		/* path name (gag) */
-};
+} __aligned(8);
 
 #if __BSD_VISIBLE
 
