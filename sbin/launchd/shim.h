999d9def3d72defc9e4b28954d1d8c8e913d1431
Matthew Macy <kmacy@freebsd.org>
2014-12-01 13:22:54 -0800

    move out launchd private xpc declarations
    (cherry picked from commit afa95c179160b1bdf98b475f1d7a50cd27fb7820)
    Signed-off-by: Scott K <scott@ixsystems.com>

--- sbin/launchd/shim.h
+++ sbin/launchd/shim.h
@@ -33,9 +33,7 @@ typedef char event_name_t[64];
 
 
 #define VQ_UPDATE VQ_FLAG0100
-#define EXNOMEM		120
-#define EXINVAL		121
-#define EXSRCH      122
+
 
 #define XPC_EVENT_ROUTINE_KEY_STREAM "XPC key stream"
 #define XPC_EVENT_ROUTINE_KEY_TOKEN "XPC key token"
@@ -128,20 +126,6 @@ malloc_size(void * ptr)
 
 	return (sallocx(ptr, 0));
 }
-
-const char *xpc_strerror(int error);
-xpc_object_t xpc_copy_entitlement_for_token(const char *, audit_token_t *);
-int xpc_pipe_routine_reply(xpc_object_t);
-int xpc_pipe_try_receive(mach_port_t, xpc_object_t *, mach_port_t *,
-						 boolean_t (*)(mach_msg_header_t *, mach_msg_header_t *), mach_msg_size_t, int);
-kern_return_t xpc_call_wakeup(mach_port_t, int);
-void xpc_dictionary_get_audit_token(xpc_object_t, audit_token_t *);
-void xpc_dictionary_set_mach_recv(xpc_object_t, const char *, mach_port_t);
-void xpc_dictionary_set_mach_send(xpc_object_t, const char *, mach_port_t);
-mach_port_t xpc_dictionary_copy_mach_send(xpc_object_t, const char *);
-xpc_object_t xpc_copy_entitlements_for_pid(pid_t);
-xpc_object_t ld2xpc(launch_data_t);
-
 
 /* domain.defs */
 kern_return_t
