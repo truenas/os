5b48ff202458c8bd3a2768c61112204398fcbab7
mav <mav@FreeBSD.org>
2014-12-03 09:32:51 +0000

    Do not corrupt the listen string when parsing it.
    This fixes problem with ctld reload when it is configured to listen on two
    portals with same IP, but different ports.
    
    MFC after:	1 week
    
    (cherry picked from commit a0aca4af786f93871c2435166f350751af9cd192)

--- usr.sbin/ctld/ctld.c
+++ usr.sbin/ctld/ctld.c
@@ -642,10 +642,11 @@ static int
 parse_addr_port(char *arg, const char *def_port, struct addrinfo **ai)
 {
 	struct addrinfo hints;
-	char *addr, *ch;
+	char *str, *addr, *ch;
 	const char *port;
 	int error, colons = 0;
 
+	str = arg = strdup(arg);
 	if (arg[0] == '[') {
 		/*
 		 * IPv6 address in square brackets, perhaps with port.
@@ -658,8 +659,10 @@ parse_addr_port(char *arg, const char *def_port, struct addrinfo **ai)
 			port = def_port;
 		} else if (arg[0] == ':') {
 			port = arg + 1;
-		} else
+		} else {
+			free(str);
 			return (1);
+		}
 	} else {
 		/*
 		 * Either IPv6 address without brackets - and without
@@ -686,9 +689,8 @@ parse_addr_port(char *arg, const char *def_port, struct addrinfo **ai)
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE;
 	error = getaddrinfo(addr, port, &hints, ai);
-	if (error != 0)
-		return (1);
-	return (0);
+	free(str);
+	return ((error != 0) ? 1 : 0);
 }
 
 int
