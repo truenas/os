5d5242d98f3b259db202bd2c873822e222458b40
Matthew Macy <kmacy@freebsd.org>
2014-12-30 15:02:33 -0800

    update path
    (cherry picked from commit 4f50795bd03281dfa879b3df2c44e4669a82c54f)
    Signed-off-by: Scott K <scott@ixsystems.com>

--- lib/libmach/test/build.sh
+++ lib/libmach/test/build.sh
@@ -1,1 +1,1 @@
-cc -I. -I../../../sys -I../../../include -g -o ipc-hello ipc-hello.c -pthread -L.. -lmach
+cc -I. -I../../../apsl/include -I../../../sys -I../../../include -g -o ipc-hello ipc-hello.c -pthread -L.. -lmach
