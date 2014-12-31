df853103098e7d89ba45c900128df4bc15831343
Matthew Macy <kmacy@freebsd.org>
2014-12-31 15:49:49 -0800

    add Makefile for ipc-hello and remove cruft
    (cherry picked from commit 6710e59d421679662cd13032b9ea8ad871b9b687)
    Signed-off-by: Scott K <scott@ixsystems.com>

--- lib/libmach/test/build.sh
+++
@@ -1,1 +0,0 @@
-cc -I. -I../../../apsl/include -I../../../sys -I../../../include -g -o ipc-hello ipc-hello.c -pthread -L.. -lmach
--- lib/libmach/test/env.sh
+++
@@ -1,1 +0,0 @@
-export LD_LIBRARY_PATH=/home/kmacy/devel/freebsd-launchd/lib/libmach/test/..
