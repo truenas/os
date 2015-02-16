be2d6bdd4dd6383859d0d4fb1a056edc9c46bcf6
Matthew Macy <kmacy@freebsd.org>
2015-02-16 14:55:46 -0800

    handle bootstrap port initializiation for root and non-root
    (cherry picked from commit 4609d7eb8d363298bbc915e84da93b78bb4309b8)
    Signed-off-by: Scott K <scott@ixsystems.com>

--- lib/libmach/mach/mach_init.c
+++ lib/libmach/mach/mach_init.c
@@ -8,6 +8,9 @@
 #include <mach/mach_types.h>
 #include <mach/mach_port.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
 
 #include <machine/mach/ndr_def.h>
 static void mach_init(void) __attribute__((constructor));
@@ -24,11 +27,43 @@ mach_port_t mach_task_self_ = MACH_PORT_NULL;
 static void
 mach_init(void)
 {
+	pid_t pid;
+	char *root_flag;
+	int root_bootstrap;
+	kern_return_t kr;
 	static bool mach_inited = false;
+	mach_port_t bport;
 
 	if (!mach_inited) {
+		root_flag = getenv("ROOT_BOOTSTRAP");
+		root_bootstrap = (root_flag != NULL) && (strcmp(root_flag, "T") == 0);
+
+		pid = getpid();
 		mach_task_self_ = task_self_trap();
         _task_reply_port = mach_reply_port();
+		if (pid != 1 && root_bootstrap == false) {
+			kr = task_get_special_port(mach_task_self_, TASK_BOOTSTRAP_PORT, &bootstrap_port);
+			if (kr != KERN_SUCCESS) {
+				fprintf(stderr, "failed to bootstrap\n");
+				abort();
+			}
+		} else {
+			kr = mach_port_allocate(mach_task_self_, MACH_PORT_RIGHT_RECEIVE, &bport);
+			if (kr != KERN_SUCCESS) 
+				abort();
+			kr = mach_port_insert_right(mach_task_self_, bport, bport, MACH_MSG_TYPE_MAKE_SEND);
+			if (kr != KERN_SUCCESS) {
+				fprintf(stderr, "failed to insert send right on bootsrap port\n");
+				abort();
+			}
+			kr = task_set_special_port(mach_task_self_, TASK_BOOTSTRAP_PORT, bport);
+			if (kr != KERN_SUCCESS) {
+				fprintf(stderr, "failed to set bootsrap port\n");
+				abort();
+			}
+			bootstrap_port = bport;
+			setenv("ROOT_BOOTSTRAP", "F", true);
+		}
 		mach_inited = true;
 	}
 }
