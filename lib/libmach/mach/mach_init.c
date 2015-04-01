c2b800687031dcc26d835126536e08e61edeca3e
Matthew Macy <kmacy@freebsd.org>
2015-03-31 22:19:00 -0700

    pid 1 expects bootstrap_port to be clear
    (cherry picked from commit bad9cbc1ca2a3ff8527754a1ad5e1dafe3587dea)
    Signed-off-by: Scott K <scott@ixsystems.com>

--- lib/libmach/mach/mach_init.c
+++ lib/libmach/mach/mach_init.c
@@ -32,8 +32,6 @@ mach_init(void)
 	int root_bootstrap;
 	kern_return_t kr;
 	static bool mach_inited = false;
-	mach_port_t bport;
-
 	if (!mach_inited) {
 		root_flag = getenv("ROOT_BOOTSTRAP");
 		root_bootstrap = (root_flag != NULL) && (strcmp(root_flag, "T") == 0);
@@ -47,22 +45,6 @@ mach_init(void)
 				fprintf(stderr, "failed to bootstrap\n");
 				abort();
 			}
-		} else {
-			kr = mach_port_allocate(mach_task_self_, MACH_PORT_RIGHT_RECEIVE, &bport);
-			if (kr != KERN_SUCCESS) 
-				abort();
-			kr = mach_port_insert_right(mach_task_self_, bport, bport, MACH_MSG_TYPE_MAKE_SEND);
-			if (kr != KERN_SUCCESS) {
-				fprintf(stderr, "failed to insert send right on bootsrap port\n");
-				abort();
-			}
-			kr = task_set_special_port(mach_task_self_, TASK_BOOTSTRAP_PORT, bport);
-			if (kr != KERN_SUCCESS) {
-				fprintf(stderr, "failed to set bootsrap port\n");
-				abort();
-			}
-			bootstrap_port = bport;
-			setenv("ROOT_BOOTSTRAP", "F", true);
 		}
 		mach_inited = true;
 	}
