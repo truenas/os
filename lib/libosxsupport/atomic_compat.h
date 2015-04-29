db6e19f31ade17b980a9d077feb80e1d7d2642f4
Jakub Klama <jakub@ixsystems.com>
2015-04-29 11:34:29 +0200

    Implement few more atomic operations used in OSX.

--- lib/libosxsupport/atomic_compat.h
+++ lib/libosxsupport/atomic_compat.h
@@ -1,5 +1,14 @@
 #include <sys/types.h>
 #include <machine/atomic.h>
+typedef int OSSpinLock;
+
 #define OSAtomicIncrement32Barrier(v) atomic_fetchadd_int((volatile int *)v, 1)
 #define OSAtomicDecrement32Barrier(v) atomic_fetchadd_int((volatile int *)v, -1)
 #define OSAtomicTestAndSetBarrier(i, v) atomic_testandset_int((volatile int *)v, i)
+#define OSAtomicAdd64(i, v) atomic_add_64((volatile int *)v, i)
+#define OSAtomicIncrement32(v) atomic_fetchadd_32((volatile int *)v, 1)
+#define OSAtomicDecrement32(v) atomic_fetchadd_32((volatile int *)v, -1)
+#define OSAtomicCompareAndSwapLongBarrier(o, i, v) atomic_cmpset_long(v, o, i)
+#define OSAtomicCompareAndSwap32Barrier(o, i, v) atomic_cmpset_32(v, o, i)
+#define OSSpinLockLock(l)
+#define OSSpinLockUnlock(l)
