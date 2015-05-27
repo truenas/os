8b7754b54e97e55fa582649b329142abb12382ee
Jakub Klama <jakub@ixsystems.com>
2015-05-26 22:09:16 -0700

    Remove unused stuff.

--- get_svn_revs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -x
-set -e
-git config core.notesRef refs/notes/freebsd/commits
-git config --add remote.origin.fetch '+refs/notes/*:refs/notes/*'
-echo  "This will take a few minutes..."
-git fetch --all
