677a0e85e4ecc9ead86b3dbf97e9ce5aa867f288
Matthew Macy <kmacy@freebsd.org>
2015-06-04 15:35:13 -0700

    ipc_kmsg_copyout_object is only used in ipc_kmsg - make it private
    (cherry picked from commit 6f0811ed6dcbc57fccafe3830e276f1dafd1c57a)
    Signed-off-by: Scott K <scott@ixsystems.com>

--- sys/sys/mach/ipc/ipc_kmsg.h
+++ sys/sys/mach/ipc/ipc_kmsg.h
@@ -373,13 +373,6 @@ extern mach_msg_return_t ipc_kmsg_copyin(
 extern void ipc_kmsg_copyin_from_kernel(
 	ipc_kmsg_t	kmsg);
 
-/* Copyout a port right returning a name */
-extern mach_msg_return_t ipc_kmsg_copyout_object(
-	ipc_space_t		space,
-	ipc_object_t		object,
-	mach_msg_type_name_t	msgt_name,
-	mach_port_name_t		*namep);
-
 /* Copyout the header and body to a user message */
 extern mach_msg_return_t ipc_kmsg_copyout(
 	ipc_kmsg_t		kmsg,
