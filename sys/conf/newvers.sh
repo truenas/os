freebsd/freebsd-src

Fix OpenSSL multiple vulnerabilities.
Security:	FreeBSD-SA-15:26.openssl
Approved by:	so

sys/conf/newvers.sh
@@ -32,7 +32,7 @@
 
 TYPE="FreeBSD"
 REVISION="10.1"
-BRANCH="RELEASE-p24"
+BRANCH="RELEASE-p25"
 if [ "X${BRANCH_OVERRIDE}" != "X" ]; then
 	BRANCH=${BRANCH_OVERRIDE}
 fi

truenas/os

(cherry picked from commit 8aa26ce550fc072b6f4c70a26b6c2b91fa534782)
Ticket: #12669
(cherry picked from commit fca2dbb8d78ee8f9b5f79bbcda7dd5d1b3c71597)

sys/conf/newvers.sh
@@ -32,7 +32,7 @@
 
 TYPE="FreeBSD"
 REVISION="9.3"
-BRANCH="RELEASE-p30"
+BRANCH="RELEASE-p31"
 if [ "X${BRANCH_OVERRIDE}" != "X" ]; then
 	BRANCH=${BRANCH_OVERRIDE}
 fi