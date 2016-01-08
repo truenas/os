fa21b13cd73779e3d38cbbad636e905b59523aba
np <np@FreeBSD.org>
2016-01-08 00:05:20 +0000

    MFC r279969:
    cxgbe(4):  fix if_media handling for T520-BT cards.  1Gbps and 100Mbps
    are valid for this card.
    
    (cherry picked from commit 3584fb4998f96435a71a81381a0031ad984abf9a)

--- sys/dev/cxgbe/t4_main.c
+++ sys/dev/cxgbe/t4_main.c
@@ -2622,9 +2622,6 @@ build_medialist(struct port_info *pi)
 
 	switch(pi->port_type) {
 	case FW_PORT_TYPE_BT_XFI:
-		ifmedia_add(media, m | IFM_10G_T, data, NULL);
-		break;
-
 	case FW_PORT_TYPE_BT_XAUI:
 		ifmedia_add(media, m | IFM_10G_T, data, NULL);
 		/* fall through */
