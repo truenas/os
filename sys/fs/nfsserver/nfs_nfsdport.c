cc7f925271f532c2b09c964b4671b9308ff53acb
rmacklem <rmacklem@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2017-05-01 21:21:04 +0000

    Change the default value of vfs.nfsd.pnfsgetdsattr to 1 so that it works for Linux clients by default.
    git-svn-id: http://svn.freebsd.org/base/projects/pnfs-planb-server@317662 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f
    (cherry picked from commit a28b7e73ff5a24f5b42a91b195c14e7ec1222a47)

--- sys/fs/nfsserver/nfs_nfsdport.c
+++ sys/fs/nfsserver/nfs_nfsdport.c
@@ -134,7 +134,7 @@ SYSCTL_INT(_vfs_nfsd, OID_AUTO, debuglevel, CTLFLAG_RW, &nfsd_debuglevel,
     0, "Debug level for NFS server");
 SYSCTL_INT(_vfs_nfsd, OID_AUTO, enable_stringtouid, CTLFLAG_RW,
     &nfsd_enable_stringtouid, 0, "Enable nfsd to accept numeric owner_names");
-static int nfsrv_pnfsgetdsattr = 0;
+static int nfsrv_pnfsgetdsattr = 1;
 SYSCTL_INT(_vfs_nfsd, OID_AUTO, pnfsgetdsattr, CTLFLAG_RW,
     &nfsrv_pnfsgetdsattr, 0, "When set getattr gets DS attributes via RPC");
 
