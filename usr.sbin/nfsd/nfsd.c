6a0ee60690add457fbbe3052d61d57afd7b9d877
John Hixson <john@ixsystems.com>
2017-10-19 16:28:21 -0700

    nfsdargs isn't used anymore

--- usr.sbin/nfsd/nfsd.c
+++ usr.sbin/nfsd/nfsd.c
@@ -1016,7 +1016,6 @@ static void
 start_server(int master, struct nfsd_nfsd_args *nfsdargp)
 {
 	char principal[MAXHOSTNAMELEN + 5];
-	struct nfsd_nfsd_args nfsdargs;
 	int status, error, vfd;
 	char hostname[MAXHOSTNAMELEN + 1], *cp;
 	char vhostname[MAXHOSTNAMELEN + 1];
