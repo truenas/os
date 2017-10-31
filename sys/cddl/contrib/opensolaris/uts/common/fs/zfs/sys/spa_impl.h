2eb8f0af9fa8f4b6720686d3d464810ebcb08037
Sean Eric Fagan <sef@ixsystems.com>
2017-10-31 11:59:11 -0700

    Bring back the scanning (scrub/resilver) tunables and how they impact things.

--- sys/cddl/contrib/opensolaris/uts/common/fs/zfs/sys/spa_impl.h
+++ sys/cddl/contrib/opensolaris/uts/common/fs/zfs/sys/spa_impl.h
@@ -184,6 +184,7 @@ struct spa {
 	uberblock_t	spa_ubsync;		/* last synced uberblock */
 	uberblock_t	spa_uberblock;		/* current uberblock */
 	boolean_t	spa_extreme_rewind;	/* rewind past deferred frees */
+	uint64_t	spa_last_io;		/* lbolt of last non-scan I/O */
 	kmutex_t	spa_scrub_lock;		/* resilver/scrub lock */
 	uint64_t	spa_scrub_inflight;	/* in-flight scrub I/Os */
 	kcondvar_t	spa_scrub_io_cv;	/* scrub I/O completion */
