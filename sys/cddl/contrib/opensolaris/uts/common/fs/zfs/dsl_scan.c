613672a683b01f8d8d27b8a13c77b0f5b84ea540
Sean Eric Fagan <sef@ixsystems.com>
2018-01-04 12:03:55 -0800

    Match zfsonlinux for the check in dsl_scan_prefetch_cb().
    I do not know if this would fix the panic, but it's clearly
    a right change to make.
    
    Ticket: #27524

--- sys/cddl/contrib/opensolaris/uts/common/fs/zfs/dsl_scan.c
+++ sys/cddl/contrib/opensolaris/uts/common/fs/zfs/dsl_scan.c
@@ -1447,8 +1447,8 @@ dsl_scan_prefetch_cb(zio_t *zio, const zbookmark_phys_t *zb, const blkptr_t *bp,
 
 	/* broadcast that the IO has completed for rate limitting purposes */
 	mutex_enter(&spa->spa_scrub_lock);
-	ASSERT3U(spa->spa_scrub_inflight, >, 0);
-	spa->spa_scrub_inflight--;
+	ASSERT3U(spa->spa_scrub_inflight, >=, BP_GET_PSIZE(bp));
+        spa->spa_scrub_inflight -= BP_GET_PSIZE(bp);
 	cv_broadcast(&spa->spa_scrub_io_cv);
 	mutex_exit(&spa->spa_scrub_lock);
 
