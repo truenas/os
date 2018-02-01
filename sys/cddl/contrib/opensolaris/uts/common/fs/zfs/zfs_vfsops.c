9a8fe3dc8c316ae14ea70542bf8d1ce8a0c5a65b
Sean Eric Fagan <sef@ixsystems.com>
2018-02-01 13:41:51 -0800

    Pull in a late commit to the pull request (2a59a6d66edd8196f30acc6d7a87c171737f9fd1, aka the 5th patch).

--- sys/cddl/contrib/opensolaris/uts/common/fs/zfs/zfs_vfsops.c
+++ sys/cddl/contrib/opensolaris/uts/common/fs/zfs/zfs_vfsops.c
@@ -1048,13 +1048,12 @@ static int
 zfsvfs_setup(zfsvfs_t *zfsvfs, boolean_t mounting)
 {
 	int error;
-	boolean_t readonly = zfsvfs->z_vfs->vfs_flag & VFS_RDONLY;
 
 	/*
 	 * Check for a bad on-disk format version now since we
 	 * lied about owning the dataset readonly before.
 	 */
-	if (!readonly &&
+	if (!(zfsvfs->z_vfs->vfs_flag & VFS_RDONLY) &&
 	    dmu_objset_incompatible_encryption_version(zfsvfs->z_os))
 		return (SET_ERROR(EROFS));
 
@@ -1070,10 +1069,13 @@ zfsvfs_setup(zfsvfs_t *zfsvfs, boolean_t mounting)
 	 * operations out since we closed the ZIL.
 	 */
 	if (mounting) {
+		boolean_t readonly;
+
 		/*
 		 * During replay we remove the read only flag to
 		 * allow replays to succeed.
 		 */
+		readonly = zfsvfs->z_vfs->vfs_flag & VFS_RDONLY;
 		if (readonly != 0)
 			zfsvfs->z_vfs->vfs_flag &= ~VFS_RDONLY;
 		else
@@ -1116,7 +1118,10 @@ zfsvfs_setup(zfsvfs_t *zfsvfs, boolean_t mounting)
 				zfsvfs->z_replay = B_FALSE;
 			}
 		}
-		zfsvfs->z_vfs->vfs_flag |= readonly; /* restore readonly bit */
+
+		/* restore readonly bit */
+		if (readonly != 0)
+			zfsvfs->z_vfs->vfs_flag |= VFS_RDONLY;
 	}
 
 	/*
