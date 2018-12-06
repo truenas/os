9e7a34ca44192ae224e340d3d18f86f736f48f7f
Sean Eric Fagan <sef@ixsystems.com>
2018-12-05 19:42:21 -0800

    Use 80 column lines; this again matches the freebsd differential proposal.
    Ticket: #55728

--- usr.sbin/mountd/mountd.c
+++ usr.sbin/mountd/mountd.c
@@ -1024,8 +1024,8 @@ mntsrv(struct svc_req *rqstp, SVCXPRT *transp)
 	case MOUNTPROC_MNT:
 	case MOUNTPROC_UMNT:
 	case MOUNTPROC_UMNTALL:
-		lookup_failed = getnameinfo(saddr, saddr->sa_len, host, sizeof host, 
-		    NULL, 0, 0);
+		lookup_failed = getnameinfo(saddr, saddr->sa_len, host,
+		    sizeof host, NULL, 0, 0);
 	}
 	getnameinfo(saddr, saddr->sa_len, numerichost,
 	    sizeof numerichost, NULL, 0, NI_NUMERICHOST);
