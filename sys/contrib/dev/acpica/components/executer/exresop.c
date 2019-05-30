e1fff20019aa877b10717b5b0abf6a8c635aa4d9
Alexander Motin <mav@FreeBSD.org>
2019-05-30 14:41:19 -0400

    Add dirty hack to make ACPI not error converting Package to int.
    There is a bug in Supermicron NVDIMM ACPI code, accessing same argument
    as both Package and Buffer.  As result, no matter what is passed there,
    ACPI code always report error.  This hack just hides the error in place
    where it is irrelevant.  It should not be a problem for correct BIOSes.
    
    Ticket:	NAS-101913

--- sys/contrib/dev/acpica/components/executer/exresop.c
+++ sys/contrib/dev/acpica/components/executer/exresop.c
@@ -627,6 +627,7 @@ AcpiExResolveOperands (
             case ACPI_TYPE_INTEGER:
             case ACPI_TYPE_STRING:
             case ACPI_TYPE_BUFFER:
+            case ACPI_TYPE_PACKAGE:
 
                 /* Valid operand */
                break;
