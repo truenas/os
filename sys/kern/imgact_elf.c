a425d621fb06fef803aa194190d781812a050fc6
mav <mav@FreeBSD.org>
2019-06-18 21:05:10 +0000

    Optimize kern.geom.conf* sysctls.
    On large systems those sysctls may generate megabytes of output.  Before
    this change sbuf(9) code was resizing buffer by 4KB each time many times,
    generating tons of TLB shootdowns.  Unfortunately in this case existing
    sbuf_new_for_sysctl() mechanism, supposed to help with this issue, is not
    applicable, since all the sbuf writes are done in different kernel thread.
    
    This change improves situation in two ways:
     - on first sysctl call, not providing any output buffer, it sets special
    sbuf drain function, just counting the data and so not needing big buffer;
     - on second sysctl call it uses as initial buffer size value saved on
    previous call, so that in most cases there will be no reallocation, unless
    GEOM topology changed significantly.
    
    Ticket:		NAS-102154
    MFC after:	1 week
    Sponsored by:	iXsystems, Inc.
    
    (cherry picked from commit a4a0ae9441093e88e463c25492eecfa02f71f107)

--- sys/kern/imgact_elf.c
+++ sys/kern/imgact_elf.c
@@ -1213,7 +1213,6 @@ static void __elfN(puthdr)(struct thread *, void *, size_t, int, size_t);
 static void __elfN(putnote)(struct note_info *, struct sbuf *);
 static size_t register_note(struct note_info_list *, int, outfunc_t, void *);
 static int sbuf_drain_core_output(void *, const char *, int);
-static int sbuf_drain_count(void *arg, const char *data, int len);
 
 static void __elfN(note_fpregset)(void *, struct sbuf *, size_t *);
 static void __elfN(note_prpsinfo)(void *, struct sbuf *, size_t *);
@@ -1347,19 +1346,6 @@ sbuf_drain_core_output(void *arg, const char *data, int len)
 	if (error != 0)
 		return (-error);
 	p->offset += len;
-	return (len);
-}
-
-/*
- * Drain into a counter.
- */
-static int
-sbuf_drain_count(void *arg, const char *data __unused, int len)
-{
-	size_t *sizep;
-
-	sizep = (size_t *)arg;
-	*sizep += len;
 	return (len);
 }
 
@@ -2128,7 +2114,7 @@ note_procstat_files(void *arg, struct sbuf *sb, size_t *sizep)
 	if (sb == NULL) {
 		size = 0;
 		sb = sbuf_new(NULL, NULL, 128, SBUF_FIXEDLEN);
-		sbuf_set_drain(sb, sbuf_drain_count, &size);
+		sbuf_set_drain(sb, sbuf_count_drain, &size);
 		sbuf_bcat(sb, &structsize, sizeof(structsize));
 		PROC_LOCK(p);
 		kern_proc_filedesc_out(p, sb, -1, filedesc_flags);
@@ -2179,7 +2165,7 @@ note_procstat_vmmap(void *arg, struct sbuf *sb, size_t *sizep)
 	if (sb == NULL) {
 		size = 0;
 		sb = sbuf_new(NULL, NULL, 128, SBUF_FIXEDLEN);
-		sbuf_set_drain(sb, sbuf_drain_count, &size);
+		sbuf_set_drain(sb, sbuf_count_drain, &size);
 		sbuf_bcat(sb, &structsize, sizeof(structsize));
 		PROC_LOCK(p);
 		kern_proc_vmmap_out(p, sb, -1, vmmap_flags);
@@ -2307,7 +2293,7 @@ __elfN(note_procstat_auxv)(void *arg, struct sbuf *sb, size_t *sizep)
 	if (sb == NULL) {
 		size = 0;
 		sb = sbuf_new(NULL, NULL, 128, SBUF_FIXEDLEN);
-		sbuf_set_drain(sb, sbuf_drain_count, &size);
+		sbuf_set_drain(sb, sbuf_count_drain, &size);
 		sbuf_bcat(sb, &structsize, sizeof(structsize));
 		PHOLD(p);
 		proc_getauxv(curthread, p, sb);
