63f710ddf209950013d596b69423dd7e6aa689b3
Andrew Walker <awalker@ixsystems.com>
2023-03-09 22:14:18 -0500

    Add FEATURE() macro

--- sys/kern/vfs_lookup.c
+++ sys/kern/vfs_lookup.c
@@ -74,6 +74,9 @@ __FBSDID("$FreeBSD$");
 #define	NAMEI_DIAGNOSTIC 1
 #undef NAMEI_DIAGNOSTIC
 
+FEATURE(rnosymlink, "supports RESOLVE_NO_SYMLINK");
+FEATURE(rbeneath, "supports RESOLVE_BENEATH");
+
 SDT_PROVIDER_DEFINE(vfs);
 SDT_PROBE_DEFINE4(vfs, namei, lookup, entry, "struct vnode *", "char *",
     "unsigned long", "bool");
